f706e3ae0f34bfcf6791530a93223097131c55d8
Sheena Wang <sheena.wang@utexas.edu>
2021-11-07 13:37:59 -0600

    PostgreSQL setup attempt 2

--- app/application.py
+++ app/application.py
@@ -4,7 +4,7 @@ Configuration for Flask App
 import os
 
 from flask import Flask, url_for, current_app
-import SQLAlchemy
+import flask.ext.SQLAlchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://jeethajxhrruzl:6dd02f314f29f7701cf0e7f6aaa7d910322f5995aaa9fd180dfb1cc8d31c2cb6@ec2-52-86-223-172.compute-1.amazonaws.com:5432/dakd6qc062hna5"
